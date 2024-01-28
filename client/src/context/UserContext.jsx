37fefd1dffdaf61ddfb127a8a6da74e0cff5c425
PPYz <90772543+ppyz8509@users.noreply.github.com>
2024-01-28 21:14:37 +0700

    add comment

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,19 +1,30 @@
-import {createContext, useEffect, useState} from 'react'
+// นำเข้าคำสั่งที่ต้องการจาก React
+import { createContext, useEffect, useState } from 'react'
 import axios from 'axios'
 
+// สร้าง Context สำหรับข้อมูลผู้ใช้
 export const UserContext = createContext();
-export const UserContextProvider = ({children}) =>{
+
+// สร้างคอมโพเนนต์ UserContextProvider ซึ่งจะเป็นผู้ให้บริการคอนเท็กซ์
+export const UserContextProvider = ({ children }) => {
+    // กำหนดตัวแปร state สำหรับเก็บชื่อผู้ใช้และ ID โดยเริ่มต้นเป็น null
     const [username, setUsername] = useState(null);
     const [id, setId] = useState(null);
-    useEffect(()=>{
-        axios.get("profile").then(response =>{
+    
+    // ใช้ useEffect เพื่อดึงข้อมูลโปรไฟล์เมื่อคอมโพเนนต์โหลด
+    useEffect(() => {
+        // ทำ HTTP GET request ไปยังเส้นทาง "/profile"
+        axios.get("profile").then(response => {
+            // เมื่อได้ข้อมูลกลับมา อัปเดต username และ id
             setId(response.data.userId);
             setUsername(response.data.username);
         });
-    },[]);
-    return(
-        <UserContext.Provider value={{username, setUsername,id, setId}}>
-        {children}
+    }, []);
+
+    // คืนค่าคอมโพเนนต์ UserContext.Provider โดยส่งค่า value ที่ประกอบด้วย username, setUsername, id, และ setId ให้กับ children
+    return (
+        <UserContext.Provider value={{ username, setUsername, id, setId }}>
+            {children}
         </UserContext.Provider>
     )
 }
